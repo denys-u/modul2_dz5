7bd275640052e9491cc9d27faffcc4c7395fbfd7
denys-u <denys.udovik@nure.ua>
2021-02-28 23:00:25 +0200

    added  class Action and created empty methods

---
+++ modul2_dz5/modul2_dz5/LogerInfo/Action.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace modul2_dz5.LogerInfo
+{
+    public class Action
+    {
+        private readonly Logger _logger = Logger.Instance;
+        public Result FirstMethod()
+        {
+
+        }
+        public Result SecondMethod()
+        {
+
+        }
+        public Result ThirdMethod()
+        {
+
+        }
+    }
+}
