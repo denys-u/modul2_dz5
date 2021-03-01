9ac7780a92cbb60957497b1055b988eb544fd66f
denys-u <denys.udovik@nure.ua>
2021-03-01 22:58:04 +0200

    filled in 3 methods in action class

--- modul2_dz5/modul2_dz5/LogerInfo/Action.cs
+++ modul2_dz5/modul2_dz5/LogerInfo/Action.cs
@@ -9,16 +9,18 @@ namespace modul2_dz5.LogerInfo
         private readonly Logger _logger = Logger.Instance;
         public Result FirstMethod()
         {
-
+            _logger.LogEventInfo($"Start method: {nameof(FirstMethod)}");
+            return new Result { Status=true};
         }
         public Result SecondMethod()
         {
-
+            _logger.LogEventInfo($"Skipped logic in method: {nameof(SecondMethod)}");
+            return new Result { Status = true };
         }
         public Result ThirdMethod()
 
         {
-
+            return new Result { Status = false, ErrorMessage="I broke a logic!" };
         }
     }
 }
