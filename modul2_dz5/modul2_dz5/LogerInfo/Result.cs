385a7f78fe6d140cf23a36733a7052f8c5fd33c0
denys-u <denys.udovik@nure.ua>
2021-03-01 22:52:15 +0200

    added class resualt

---
+++ modul2_dz5/modul2_dz5/LogerInfo/Result.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace modul2_dz5.LogerInfo
+{
+    public class Result
+    {
+        public bool Status { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
