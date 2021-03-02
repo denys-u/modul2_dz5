3114e7c3c9daf830e4018433f130271c5438cf68
denys-u <denys.udovik@nure.ua>
2021-03-02 18:57:23 +0200

    Created class starter

---
+++ modul2_dz5/modul2_dz5/LogerInfo/Starter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace modul2_dz5.LogerInfo
+{
+    public class Starter
+    {
+        const int minRandom = 1;
+        const int maxRandom = 4;
+        private const string LoggerFilePath = "log.txt";
+        private readonly Logger _logger;
+        private readonly Action _action;
+        private readonly FileService _fileService;
+
+        public Starter()
+        {
+            _logger = Logger.Instance;
+            _action = new Action();
+            _fileService = new FileService();
+        }
+        public void Run()
+        {
+            var random = new Random();
+
+            for(var i=100; i<100;i++)
+            {
+                var result = new Random();
+                var randomAction = random.Next(minRandom, maxRandom);
+
+                switch (randomAction)
+                {
+                    case 1:
+                        _ = _action.FirstMethod();
+                        break;
+                    case 2:
+                        _ = _action.SecondMethod();
+                        break;
+                    case 3:
+                        _ = _action.ThirdMethod();
+                        break;
+                }
+
+                if (!result.Status)
+                {
+                    continue;
+                }
+                _logger.LogEventError($"Action failed by a reason: {result.ErrorMessage}");
+            }
+            _fileService.Write(LoggerFilePath, _logger.Logs);
+        }
+    }
+}
--- modul2_dz5/modul2_dz5/Program.cs
+++ modul2_dz5/modul2_dz5/Program.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using modul2_dz5.LogerInfo;
+using System;
 
 namespace modul2_dz5
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var starter = new Starter();
+            starter.Run();
         }
     }
 }
