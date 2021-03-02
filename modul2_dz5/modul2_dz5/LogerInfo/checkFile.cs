3dac8e211bb8069a6d63c16ce2c025f33848cdc5
denys-u <denys.udovik@nure.ua>
2021-03-02 20:41:46 +0200

    created checkFile

---
+++ modul2_dz5/modul2_dz5/LogerInfo/checkFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace modul2_dz5.LogerInfo
+{
+    public class CheckFile
+    {
+        FileInfo fileInfo = new FileInfo(LoggerFilePath);
+        
+        public static string LoggerFilePath { get; set; }
+
+    }
+}
